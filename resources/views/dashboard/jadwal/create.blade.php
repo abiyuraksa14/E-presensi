e5f9a0875eb10352a1f1f6158a26a97ac3b51126
Muhammad Dwiki Yudhistira <yudhistiradwiki17@gmail.com>
2024-07-18 21:45:45 +0700

    fixing master data

--- resources/views/dashboard/jadwal/create.blade.php
+++ resources/views/dashboard/jadwal/create.blade.php
@@ -12,56 +12,54 @@
             <!-- General Form Elements -->
             <form action="{{ route('jadwal.store') }}" method="POST">
                 @csrf
-              <div class="row mb-3">
-                <label for="inputText" class="col-sm-2 col-form-label">Id jadwal</label>
-                <div class="col-sm-10">
-                  <input type="text" class="form-control" name="id_jadwal">
-                </div>
-              </div>
-              <div class="row mb-3">
+              <div class="mb-3 row">
                 <label for="inputText" class="col-sm-2 col-form-label">hari</label>
                 <div class="col-sm-10">
                   <input type="text" class="form-control" name="hari">
                 </div>
               </div>
-                <div class="row mb-3">
+                <div class="mb-3 row">
                     <label for="jam_mulai" class="col-sm-2 col-form-label">jam_mulai</label>
                     <div class="col-sm-10">
                     <input type="time" class="form-control" name="jam_mulai">
                     </div>
               </div>
-              <div class="row mb-3">
+              <div class="mb-3 row">
                 <label for="jam_akhir" class="col-sm-2 col-form-label">jam_akhir</label>
                 <div class="col-sm-10">
                   <input type="time" class="form-control" name="jam_akhir">
                 </div>
               </div>
-              <div class="row mb-3">
+              <div class="mb-3 row">
                 <label for="inputText" class="col-sm-2 col-form-label">id_matkul</label>
                 <div class="col-sm-10">
-                  <input type="text" class="form-control" name="id_matkul">
+                  <select name="id_matkul" id="id_matkul" class="form-control">
+                    @foreach($matkul as $mtk)
+                        <option value="{{$mtk->id}}">{{$mtk->kd_matkul}} - {{$mtk->nama_matkul}}</option>
+                    @endforeach
+                  </select>
                 </div>
               </div>
-              <div class="row mb-3">
+              <div class="mb-3 row">
                 <label for="inputText" class="col-sm-2 col-form-label">tahun akademik</label>
                 <div class="col-sm-10">
                   <input type="text" class="form-control" name="tahun_akademik">
                 </div>
               </div>
-              <div class="row mb-3">
+              <div class="mb-3 row">
                 <label for="inputText" class="col-sm-2 col-form-label">jumlah peserta</label>
                 <div class="col-sm-10">
                   <input type="text" class="form-control" name="jumlah_peserta">
                 </div>
               </div>
-              {{-- <div class="row mb-3">
+              {{-- <div class="mb-3 row">
                 <label for="inputText" class="col-sm-2 col-form-label">Buat Barcode</label>
                 <div class="col-sm-10">
                   <input type="text" class="form-control" name="barcode">
                 </div>
               </div> --}}
 
-              <div class="row mb-3">
+              <div class="mb-3 row">
                 <label class="col-sm-2 col-form-label"></label>
                 <div class="col-sm-10">
                   <button type="submit" class="btn btn-primary">Submit</button>
